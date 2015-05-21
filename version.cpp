7b91f33f08c8e3a6b32d0fc2203260fe2a156884
Vijay Pradeep <vijaypradeep@google.com>
2015-05-20 18:29:44 -0700

    Incrementing version num

--- version.cpp
+++ version.cpp
@@ -1,7 +1,7 @@
 // This is an example file that contains a version number that
 // should be incremented on every new feature change.
 
-static const int VERSION_NUM = 1;
+static const int VERSION_NUM = 2;
 
 // **************
 // Feature A
